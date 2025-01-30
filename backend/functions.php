7cf0dc52183a07208b49d16380a9c99af99313a3
AKAJellyBean <dinethkaveesha114@gmail.com>
2025-01-31 00:51:50 +0530

    Implement backend user registration functionality

---
+++ backend/functions.php
@@ -0,0 +1,28 @@
+<?php
+require 'config.php';
+
+// Function to insert user into the database
+function addUser($first_name, $last_name, $email, $phone, $nic, $street_address, $city, $province, $password) {
+    global $conn;
+    
+    // Hash password
+    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+    // Use double quotes for the "user" table to avoid conflicts with reserved keywords
+    $query = 'INSERT INTO "user" (first_name, last_name, email, phone, nic, street_address, city, province, password) 
+              VALUES (:first_name, :last_name, :email, :phone, :nic, :street_address, :city, :province, :password)';
+    
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':first_name', $first_name);
+    $stmt->bindParam(':last_name', $last_name);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':phone', $phone);
+    $stmt->bindParam(':nic', $nic);
+    $stmt->bindParam(':street_address', $street_address);
+    $stmt->bindParam(':city', $city);
+    $stmt->bindParam(':province', $province);
+    $stmt->bindParam(':password', $hashed_password);
+
+    return $stmt->execute();
+}
+?>
