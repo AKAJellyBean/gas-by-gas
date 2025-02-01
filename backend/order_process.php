15537e246ccf005248dffbd9b4c593e4c46ee65d
AKAJellyBean <dinethkaveesha114@gmail.com>
2025-02-01 10:27:51 +0530

    Add guest gas request feature

--- backend/order_process.php
+++ backend/order_process.php
@@ -5,29 +5,67 @@
 
     // taking input from the form
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $user_id = $_SESSION['user_id'];
-        $outlet_id = $_POST['outlet'];
-        $gas_type = $_POST['gas_type'];
-        $quantity = $_POST['quantity'];
 
-        // get outlet info by id
-        $outlet = getOutletInfoById($outlet_id);
-        $outlet_name = $outlet['outlet_name']; // outlet name
+        // check if the user is logged in
+        if(!isset($_SESSION['user_id'])) {
+            // if user is not logged in
+            // handle the guest user requests
+            // get the input from the form
+            $first_name = $_POST['first_name'];
+            $last_name = $_POST['last_name'];
+            $phone = $_POST['phone'];
+            $nic = $_POST['nic'];
 
-        // get gas data from the database
-        $gas = getGasNameById($gas_type);
-        $gas_name = "{$gas['gas_name']}kg"; // gas name
+            $user_id = "guest";
+            $outlet_id = $_POST['outlet'];
+            $gas_type = $_POST['gas_type'];
+            $quantity = $_POST['quantity'];
 
-        // generate token
-        $token = generateToken($outlet_id, $user_id, $gas_name);
+            // get outlet info by id
+            $outlet = getOutletInfoById($outlet_id);
 
-        // generate pickup date
-        $pickup_date = date('Y-m-d', strtotime('+7 day'));
+            // get gas data from the database
+            $gas = getGasNameById($gas_type);
+            $gas_name = "{$gas['gas_name']}kg"; // gas name
 
-        // insert request into the database
-        $created_at = date('Y-m-d');
-        insertRequest($user_id, $outlet_id, $token, "pending", $pickup_date, $created_at);
+            // generate token
+            $token = generateToken($outlet_id,$user_id, $gas_name);
 
-        echo $token;
+            // generate pickup date
+            $pickup_date = date('Y-m-d', strtotime('+7 day'));
+
+            // insert request into the database
+            $created_at = date('Y-m-d');
+            insertGuestRequest($first_name, $last_name, $phone, $nic, $outlet_id, $token, "pending", $pickup_date, $created_at);
+        } else {
+
+            // if user is logged in
+            $user_id = $_SESSION['user_id'];
+            $outlet_id = $_POST['outlet'];
+            $gas_type = $_POST['gas_type'];
+            $quantity = $_POST['quantity'];
+
+            // get outlet info by id
+            $outlet = getOutletInfoById($outlet_id);
+            $outlet_name = $outlet['outlet_name']; // outlet name
+
+            // get gas data from the database
+            $gas = getGasNameById($gas_type);
+            $gas_name = "{$gas['gas_name']}kg"; // gas name
+
+            // generate token
+            $token = generateToken($outlet_id, $user_id, $gas_name);
+
+            // generate pickup date
+            $pickup_date = date('Y-m-d', strtotime('+7 day'));
+
+            // insert request into the database
+            $created_at = date('Y-m-d');
+            insertRequest($user_id, $outlet_id, $token, "pending", $pickup_date, $created_at);
+
+            echo $token;
+        }
+
+        
     }
 ?>
